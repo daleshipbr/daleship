8c9a0ff4151bc30a0080a8b8dfa03c8d063d5ce1
vinicius costa <vinicius@festalab.com>
2020-01-25 23:34:51 -0300

    create model user

---
+++ db/migrate/20200126023437_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :job_title
+      t.string :type
+      t.references :company, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
