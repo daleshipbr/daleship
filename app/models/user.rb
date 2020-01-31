33b096f7c372ea587c33046173a82ae622d8459f
vinicius costa <vinicius@festalab.com>
2020-01-31 00:19:44 -0300

    added validation methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,62 @@
 class User < ApplicationRecord
+
+  attr_accessor :remember_token, :reset_token
+
   belongs_to :company
+  has_many :freights
+
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 255 }
+  validates :job_title, presence: true, length: { maximum: 255 }
+  validates :user_type, presence: true, inclusion: { in: ["shipper", "carrier"] }
+
+  # implementar email de boas vindas
+  # after_commit -> { UserMailer.welcome(self).deliver_later }, on: :create
 
   has_secure_password
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update(remember_digest: nil,
+           facebook_token: nil,
+           facebook_expires_at: nil,
+           google_token: nil,
+           google_expires_at: nil)
+  end
+
+  def reset
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    reset
+    UserMailer.password_reset(self, reset_token).deliver_later
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 24.hours.ago
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_024245) do
+ActiveRecord::Schema.define(version: 2020_01_31_025754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,13 @@ ActiveRecord::Schema.define(version: 2020_01_26_024245) do
     t.bigint "company_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone"
+    t.string "remenber_digest"
+    t.boolean "admin", default: false
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
+    t.boolean "accepted_terms_of_service"
+    t.datetime "destroyed_at"
     t.index ["company_id"], name: "index_users_on_company_id"
   end
 
